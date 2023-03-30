4a5e07c86d2ab5853d412004e7e7348c0a410352
shamsi <sarah.shamsi90@gmail.com>
2023-03-29 21:50:15 -0700

    let const ex completed

---
+++ letConstEx.js
@@ -0,0 +1,21 @@
+const PI = 3.14;
+
+// What is the difference between var and let?
+
+/*var variable can be redeclared and reassigned amd it has function scope 
+while let variable cannot be reassigned and it has code block scope.
+*/
+
+// What is the difference between var and const?
+
+/* var variable can be redeclared and reassigned while const variable can nether be redeclred 
+nor reassigned and has code block scope.
+ */
+
+//What is the difference between let and const ?
+/* let variable can be reassigned while const cannot be reassigned. */
+
+// What is hoisting?
+// When var variable declaration is moved at the top of the code compilation however its value remains undefined until value
+// is assigned. This is not the case in the let and const variable declaration. function declaration are also hoisted before defining in the 
+// codebase.
